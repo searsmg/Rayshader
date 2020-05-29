1c7408f1ea46e1d2fba9a1959bba00e1b043c081
searsmg <megan.sears@colostate.edu>
2020-05-29 16:31:35 -0600

    Adding rayshader for CARE

---
+++ Rayshader_CARE.R
@@ -0,0 +1,17 @@
+library(rayshader)
+library(rgdal)
+
+#Here, I load a map with the raster package.
+#loadzip = tempfile() 
+#download.file("C:/Users/sears/Desktop/USGS_13_n39w112.tif", loadzip)
+#setwd("C:/Users/sears/Desktop/USGS_13_n39w112.tif")
+localtif = raster::raster("C:/Users/sears/Desktop/USGS_13_n39w112.tif")
+#unlink(loadzip)
+
+#And convert it to a matrix:
+elmat = raster_to_matrix(localtif)
+
+#We use another one of rayshader's built-in textures:
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  plot_map()
